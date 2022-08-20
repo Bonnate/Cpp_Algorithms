17e7947e080efa7f89a5b4fee97133f458a69211
Bonnate <vldkdhtudrjs@gmail.com>
2022-08-21 05:44:31 +0900

    0821

--- 2447__별찍기 - 10/main.cpp
+++ 2447__별찍기 - 10/main.cpp
@@ -1,27 +1,43 @@
 #include <iostream>
-
 using namespace std;
+
+void foo(int i, int j, int N)
+{
+    // 3*3의 크기에서 중심인경우 [1,1] 공백 출력
+    if ((i / N) % 3 == 1 && (j / N) % 3 == 1)
+    {
+        cout << i << j << N;
+        cout << ' ';
+    }
+    else
+    {
+        //중심이 아니고, 가장 작은 형태인경우 *을 출력한다.
+        if (N / 3 == 0)
+        {
+            cout << '*';
+        }
+        else
+        {
+            //작은 문제로 분할한다.
+            foo(i, j, N / 3);
+        }
+    }
+}
+
 int main()
 {
-    int n, t;
+    int N;
 
-    cin >> n;
+    cin >> N;
 
-    cout << '[' << n / 10000 * 10000 << ']' << endl;
-
-    t = n % 10000 / 1000;
-    if (t != 0)
-        cout << '[' << t * 1000 << ']' << endl;
-
-    t = n % 1000 / 100;
-    if (t != 0)
-        cout << '[' << t * 100 << ']' << endl;
-
-    t = n % 100 / 10;
-    if (t != 0)
-        cout << '[' << t * 10 << ']' << endl;
-
-    t = n % 10;
-    if (t != 0)
-        cout << '[' << t << ']' << endl;
+    //출력은 총 n^2번 한다. (정사각형 모양)
+    // 2중 for문으로 foo를 실행하도록 한다.
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            foo(i, j, N);
+        }
+        cout << endl;
+    }
 }
