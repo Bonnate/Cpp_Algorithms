b3f6e69718c791f307828df13992985187d2b68c
PARKSEUNGGEUN <vldkdhtudrjs@gmail.com>
2023-09-27 08:58:38 +0900

    인프런 3번 문제

---
+++ Inflearn/3/AA/AA/AA.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+/*
+int main()
+{
+	int N; // 입력값
+	int sum = 0; // 총 합
+	queue<int> properDivisors; // 진약수 큐
+
+	scanf_s("%d", &N);
+
+	for (int i = 1; i <= N / 2; ++i)
+		if (N % i == 0)
+		{
+			properDivisors.push(i);
+			sum += i;
+		}
+
+	// 마지막 값을 제외하고 + 포맷으로 출력
+	while (properDivisors.size() > 1)
+	{
+		printf("%d + ", properDivisors.front());
+		properDivisors.pop();
+	}
+
+	// 마지막 값은 = 포맷과 총 합 출력
+	printf("%d = %d", properDivisors.front(), sum);
+	properDivisors.pop();
+}
+*/
+
+int main()
+{
+	int N; // 입력값
+	int sum = 1; // 총 합
+
+	scanf_s("%d", &N);
+
+	printf("%d", 1);
+	for (int i = 2; i <= N / 2; ++i)
+		if (N % i == 0)
+		{
+			printf(" + %d", i);
+			sum += i;
+		}
+
+	printf(" = %d", sum);
+}
