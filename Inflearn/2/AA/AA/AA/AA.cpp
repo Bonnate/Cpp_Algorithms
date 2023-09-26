4b54e5b027e56e6afaf7c90db39504e29bd60b5d
PARKSEUNGGEUN <vldkdhtudrjs@gmail.com>
2023-09-27 00:34:22 +0900

    인프런 2번째 문제

---
+++ Inflearn/2/AA/AA/AA/AA.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int A, B; // 입력 A, B
+	int sum = 0; // 합
+
+	cin >> A >> B; // 입력
+
+	for (int i = A; i < B; ++i)
+	{
+		printf("%d + ", i);
+		sum += i;
+	}
+
+	printf("%d = %d", B, sum += B);
+}
